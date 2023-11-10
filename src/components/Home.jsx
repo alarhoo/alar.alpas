e282386fe97179b70173cd53ca091a44c9b4431c
manja1227 <manjunatha1227@gmail.com>
2023-11-11 03:44:46 +0530

    update

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div >
+      <img src='https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg' alt='alpas-bg' width="100%" height="99%" />
+    </div>
+  );
+}
+
+export default Home;
